7b5f37ceba2008aa4cbaff1701c31424f2f75b1b
Yuu-Ao-Deki <mr2sw20na5@icloud.com>
2025-06-21 22:10:37 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,8 @@
+const weight = 68;
+const height = 1.7;
+
+const bmi = weight / (height * height);
+
+console.log("Weight:", weight, "kg");
+console.log("Height:", height, "m");
+console.log("BMI:", bmi.toFixed(2));
